7339b82a62fe4244751d03e2d58502255e7054dd
Kailash Singh <singhkailash.sk9@gmail.com>
2024-02-09 19:32:04 +0530

    Separate command handler logic

--- app.js
+++ app.js
@@ -2,41 +2,15 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 import { Client, GatewayIntentBits } from 'discord.js';
-import { testCmd } from './commands/test.js';
-import { helpCmd } from './commands/help.js';
-import { memeCmd } from './commands/meme.js';
-import { quoteCmd } from './commands/quote.js';
+import { handler } from './commandsHandler.js';
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-const prefix = '+';
-
-client.once('ready', () => {
-	console.log(`${client.user.tag} has logged in`);
-});
-
 client.on('messageCreate', message => {
-    if (message.author.bot) return;
-
-    if (message.content.startsWith(prefix)) {
-		const [cmd_name, ...args] = message.content.trim().substring(prefix.length).split(/\s+/);
-        switch (cmd_name) {
-            case 'test':
-                testCmd(message);
-                break;
-            case 'help':
-                helpCmd(message, prefix);
-                break;
-            case 'meme':
-                memeCmd(message);
-                break;
-            case 'quote':
-                quoteCmd(message);
-                break;
-            default:
-                message.channel.send(`Not a valid command. Please try ${prefix}help`);
-            }
-        }
+    handler(message);
+});
+client.once('ready', () => {
+    console.log(`${client.user.tag} has logged in`);
 });
 
 client.login(process.env.BOT_TOKEN);
---
+++ commandsHandler.js
@@ -0,0 +1,32 @@
+import { testCmd } from './commands/test.js';
+import { helpCmd } from './commands/help.js';
+import { memeCmd } from './commands/meme.js';
+import { quoteCmd } from './commands/quote.js';
+
+const prefix = '+';
+
+const handler = (message) => {
+    if (message.author.bot) return;
+
+    if (message.content.startsWith(prefix)) {
+        const [cmd_name, ...args] = message.content.trim().substring(prefix.length).split(/\s+/);
+        switch (cmd_name) {
+            case 'test':
+                testCmd(message);
+                break;
+            case 'help':
+                helpCmd(message, prefix);
+                break;
+            case 'meme':
+                memeCmd(message);
+                break;
+            case 'quote':
+                quoteCmd(message);
+                break;
+            default:
+                message.channel.send(`Not a valid command. Please try ${prefix}help`);
+        }
+    };
+}
+
+export { handler }
